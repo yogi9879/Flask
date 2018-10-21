5187f7e4f28351005b3d89966e7678dca249bc94
yogi9879 <32189018+yogi9879@users.noreply.github.com>
2018-10-22 00:04:23 +0530

    ML1

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-from flask import Flask, flash, redirect, render_template, request, session, abort
- 
+ffrom flask import Flask, render_template, request,make_response
+from werkzeug.datastructures import ImmutableMultiDict
+from werkzeug import secure_filename
+import pandas as pd
+import MLfun
 app = Flask(__name__)
- 
-@app.route("/")
-def index():
-    return "Flask App!"
- 
-@app.route("/templates")
-def hello():
-    return render_template(
-        'Html.html')
-if __name__ == "__main__":
-    app.run()
+
+@app.route('/upload')
+def upload_file():
+   return render_template('upload.html')
+	
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file1():
+   if request.method == 'POST':
+      f = request.files['file']
+     # data=database(f)
+      dataset=pd.read_csv(f)
+      results=MLfun.Model(dataset)
+      #f.save(secure_filename(f.filename))
+      return 'file uploaded successfully' + str(results)
+
 
