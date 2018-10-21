b010ce4130c5b1dd787e99cb26e37098adbcc430
yogi9879 <32189018+yogi9879@users.noreply.github.com>
2018-10-22 00:17:01 +0530

    simpl

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template, request,make_response
-from werkzeug.datastructures import ImmutableMultiDict
-from werkzeug import secure_filename
-import pandas as pd
-import MLfun
+from flask import Flask #, render_template, request,make_response
+#from werkzeug.datastructures import ImmutableMultiDict
+#from werkzeug import secure_filename
+#import pandas as pd
+#import MLfun
 app = Flask(__name__)
 
 @app.route('/')
 def upload_file():
    return "Hello"
+'''
 	
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file1():
@@ -18,6 +19,6 @@ def upload_file1():
       results=MLfun.Model(dataset)
       #f.save(secure_filename(f.filename))
       return 'file uploaded successfully' + str(results)
-
+'''
 if __name__ == '__main__':
-   app.run(debug=True)
+   app.run()
