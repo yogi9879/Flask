faf24eba7470df7e18c3bae0a5b5c85ec5d51352
yogi9879 <32189018+yogi9879@users.noreply.github.com>
2018-10-25 15:36:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask , render_template#, request#,make_response
 #from werkzeug import secure_filename
-#import pandas as pd
+import pandas as pd
 import numpy as np
 #from sklearn import datasets
 #import MLfun
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def upload_file():
-   return "Hello yogesh u gotcha yuhu2"
+   return "Hello yogesh u gotcha yuhu22"
 '''
 	
 @app.route('/uploader', methods = ['GET', 'POST'])
