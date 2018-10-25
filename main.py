38d4c9361ac7f0f3847ebac33cee37d94adfd8be
yogi9879 <32189018+yogi9879@users.noreply.github.com>
2018-10-25 23:13:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask , render_template#, request#,make_response
 #from werkzeug import secure_filename
 #import pandas as pd
-import numpy as np
+#import numpy as np
 #from sklearn import datasets
 #import MLfun.Model
-import scipy
+#import scipy
 app = Flask(__name__)
 
 @app.route('/')
